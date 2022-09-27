3a75b6b5b79c04043cf63a812f12e528ad76a374
Leyght <savier811@gmail.com>
2022-09-28 12:20:43 +1300

    Initial Commit

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import "../styles/style.css";
+import "../styles/contact.css";
 
 const Contact = () => {
     return (
-        <div>
-            
+        <div className="contact-page">
+            <div className="contact-page-content">
+                <div className="contact-page-form">
+
+                </div>
+                <div className="contact-page-text">
+
+                </div>
+            </div>
         </div>
     )
 }
