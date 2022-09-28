d86e796dfd402cdd075455c0aa5b7d77dbad8817
Leyght <savier811@gmail.com>
2022-09-28 15:44:22 +1300

    Complete and Responsive

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,20 +1,73 @@
 import React from "react";
 import "../styles/style.css";
 import "../styles/contact.css";
+import { useState } from "react";
 
 const Contact = () => {
-    return (
-        <div className="contact-page">
-            <div className="contact-page-content">
-                <div className="contact-page-form">
+  const [subject, setSubject] = useState("");
+  const [bodyText, setBody] = useState("");
 
-                </div>
-                <div className="contact-page-text">
-
-                </div>
-            </div>
+  console.log(subject, "sadjfkldsahfkhsadkld", bodyText)
+  return (
+    <div className="contact-page">
+      <div className="contact-page-content">
+        <div className="contact-page-form">
+          <div className="contact-header text">
+            <i>Contact Me</i>
+          </div>
+          <div className="contact-form-subject">
+            <span className="text contact-form-label">
+              <b>
+                <i>Subject: </i>
+              </b>
+            </span>
+            <input
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+          </div>
+          <div className="contact-form-body">
+            <span className="text contact-form-label">
+              <b>
+                <i>Body: </i>
+              </b>
+            </span>
+            <textarea
+              value={bodyText}
+              onChange={(e) => setBody(e.target.value)}
+            />
+          </div>
+          <div className="contact-form-submission">
+            <a
+              href={
+                "mailto:wesleyhamish@hotmail.com?subject=" +
+                subject +
+                "&body=" +
+                bodyText
+              }
+            >
+              <div className="contact-form-submission-button">
+                <i>
+                  <b>Send</b>
+                </i>
+              </div>
+            </a>
+          </div>
         </div>
-    )
-}
+        <div className="contact-page-text">
+          <div className="contact-header text">
+            <i>Details</i>
+          </div>
+          <div className="contact-text-info text">
+            <i>
+              <b>Phone:</b> 022 471 1842 <br />{" "}
+              <b>Email: wesleyhamish@hotmail.com</b>
+            </i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
